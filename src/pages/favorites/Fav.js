896fd854ea8526e32c85547f1b9cb7d489ff7083
harnishdangi <harnishdangi@gmail.com>
2025-07-19 15:52:19 +0530

    @make the admin panel

---
+++ src/pages/favorites/Fav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Footer, NavBar } from '../../components'
+
+
+function Fav() {
+    return (
+        <>
+        <NavBar/>
+        <Footer/>
+        </>
+    )
+}
+
+export default Fav
